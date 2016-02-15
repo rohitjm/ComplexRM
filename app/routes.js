1d01d23673f856c8358ee3dd7abfe3fb55a2a3e9
ROHIT MATHEW <rohitjm@gmail.com>
2016-02-15 12:30:05 -0800

    Change db url to local mongoDB instance, make routes.js for backend routing

---
+++ app/routes.js
@@ -0,0 +1,17 @@
+var Nerd = require('./models/nerd');
+
+module.exports = function(app){
+
+  app.get('/api/nerds', function(req,res){
+    Nerd.find(function(err,nerds){
+      if(err){
+        res.send(err);
+      }
+      res.json(nerds);
+    });
+  });
+
+  app.get('*', function(req,res){
+    res.sendFile('./public/views/index.html');
+  });
+};
